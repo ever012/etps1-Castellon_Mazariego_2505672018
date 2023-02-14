26167b8e5b542bf57c873243a904f5e371e16fc9
Ever Enrique Castellon Mazariego <2505672018@mail.utec.edu.sv>
2023-02-13 18:35:37 -0600

    pequeños cambios

--- GUIA_2_ETPS1/app/src/main/java/sv/edu/utec/guia_2_etps1/TercerActivity.java
+++ GUIA_2_ETPS1/app/src/main/java/sv/edu/utec/guia_2_etps1/TercerActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+
 public class TercerActivity extends AppCompatActivity {
     EditText precio_prod;
     TextView resultado;
@@ -24,14 +26,16 @@ public class TercerActivity extends AppCompatActivity {
     }
     public void calcular(View v){
         try{
+            resultado.setText("");
             Double precio = Double.parseDouble(precio_prod.getText().toString());
             Double calculoIVA;
             Double IVA = 0.134; //13.4%
             String resul = "";
+            DecimalFormat df = new DecimalFormat("0.00"); //Especifico que la salida de un formato con solo 2 decimales
                 calculoIVA =  precio * IVA;
                 resul = String.valueOf(precio + calculoIVA);
-                resultado.append("El IVA es de: $" + calculoIVA + "\n");
-                resultado.append("El precio final con IVA: $" + resul);
+                resultado.append("El IVA es de: $" + df.format(calculoIVA) + "\n");
+                resultado.append("El precio final con IVA: $" + df.format(Double.valueOf(resul))); //lo convierto a double porque sino me da error
         }catch (Exception e)
         {
             resultado.setText("Debe introducir el precio del producto");
