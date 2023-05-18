cc405a0609386f81e092ddb65123a28da41d4cb2
Ever Enrique Castellon Mazariego <2505672018@mail.utec.edu.sv>
2023-05-17 18:43:05 -0600

    manteniedo

--- app/src/main/java/sv/edu/utec/menu/MainActivity.java
+++ app/src/main/java/sv/edu/utec/menu/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView.OnNavigationItemSelectedListener {item ->
+                when(item.itemId) {
+            R.id.item1 -> {
+                // Respond to navigation item 1 click
+                true
+            }
+            R.id.item2 -> {
+                // Respond to navigation item 2 click
+                true
+            }
+        else -> false
+        }
+        }
+
+
     }
+
+
 }
